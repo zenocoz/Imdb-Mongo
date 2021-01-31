db84fd0a72cd4059c053dce81ecba11122d59467
zenocoz <federico.soncini@gmail.com>
2021-01-31 15:52:48 +0100

    film crud completed

--- src/controller/filmsController.js
+++ src/controller/filmsController.js
@@ -20,22 +20,60 @@ exports.addFilm = async (req, res, next) => {
     next(error)
   }
 }
-exports.getFilm = async (req, res, next) => {}
-exports.getAllFilms = async (req, res, next) => {}
+exports.getFilm = async (req, res, next) => {
+  try {
+    const film = await db.Film.findById(req.params.filmId).exec()
+    if (!film) {
+      throw new ApiError(400, "Film not found")
+    } else {
+      res.status(201).send({ data: film })
+    }
+  } catch (error) {
+    next(error)
+  }
+}
+exports.getAllFilms = async (req, res, next) => {
+  try {
+    const films = await db.Film.find().exec()
+    if (!films) {
+      throw new ApiError(400, "There are no films in the database")
+    } else {
+      res.status(201).send({ data: films })
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
 exports.editFilmData = async (req, res, next) => {
   try {
     const modifiedFilm = await db.Film.findByIdAndUpdate(
       req.params.filmId,
-      req.body
+      req.body,
+      { new: true }
     )
-    if (modifiedFilm) {
-      res.status(201).json({ data: modifiedFilm })
-    } else {
+    if (!modifiedFilm) {
       throw new ApiError(401, "Film not found")
+    } else {
+      res.status(201).json({ data: modifiedFilm })
     }
   } catch (error) {
     console.log("error in modifying film data", error)
     next(error)
   }
 }
-exports.deleteFilm = async (req, res, next) => {}
+
+exports.deleteFilm = async (req, res, next) => {
+  try {
+    const deletedFilm = await db.Film.findByIdAndDelete(req.params.filmId)
+    console.log(deletedFilm)
+    if (deletedFilm) {
+      res.status(201).send("film deleted")
+    } else {
+      throw new ApiError(401, "Film not found")
+    }
+  } catch (error) {
+    console.log("error in deleting the film", error)
+    next(error)
+  }
+}
--- src/routes/films/index.js
+++ src/routes/films/index.js
@@ -1,15 +1,20 @@
 const router = require("express").Router()
 const cloudinaryMulter = require("../../middlewares/cloudinary")
 
-const { addFilm, editFilmData } = require("../../controller/filmsController")
+const {
+  addFilm,
+  editFilmData,
+  getAllFilms,
+  getFilm,
+  deleteFilm,
+} = require("../../controller/filmsController")
 const { filmSchema, validateBody } = require("../../middlewares/validator")
 
 router.post("/", validateBody(filmSchema), addFilm)
 router.put("/:filmId", validateBody(filmSchema), editFilmData)
-// router.get("/", get);
-// router.get("/:postId", getSinglePost);
-// router.put("/:postId", validateBody(postSchema), modifyPost);
-// router.delete("/:postId", deletePost);
+router.get("/", getAllFilms)
+router.get("/:filmId", getFilm)
+router.delete("/:filmId", deleteFilm)
 // router.post("/:postId/picture", cloudinaryMulter.single("post"), postImage);
 
 module.exports = router
